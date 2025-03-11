cbcb1c8679baebd76a94697b955e6d96195ef2e7
Daniel Uwadi <danieluwadi@yahoo.com>
2025-03-11 16:43:17 +0100

    Refactored route params

--- src/app/api/products/[product_id]/route.ts
+++ src/app/api/products/[product_id]/route.ts
@@ -3,12 +3,18 @@ import { productDB } from "@/app/lib/database";
 import { getServerSession } from "next-auth";
 import authOptions from "../../auth/[...nextauth]/authOptions";
 
+interface NextRequestContext {
+  params: {
+    product_id: string;
+  };
+}
+
 export async function GET(
   req: Request,
-  { params }: { params: { product_id: string } }
+  context: NextRequestContext
 ) {
   try {
-    const { product_id } = params; // Extract product ID from URL
+    const { product_id } = context.params; // Extract product ID from URL
 
     if (!product_id) {
       return NextResponse.json({ message: "Product ID is required" }, { status: 400 });
--- src/app/api/session/[user_id]/[chat_id]/route.ts
+++ src/app/api/session/[user_id]/[chat_id]/route.ts
@@ -3,12 +3,19 @@ import { sessionDB } from "@/app/lib/database";
 import { getServerSession } from "next-auth";
 import authOptions from "../../../auth/[...nextauth]/authOptions";
 
+interface NextRequestContext {
+  params: {
+    user_id: string;
+    chat_id: string;
+  };
+}
+
 export async function GET(
   req: Request,
-  { params }: { params: { user_id: string, chat_id: string } }
+  context: NextRequestContext
 ) {
   try {
-    const { user_id, chat_id } = params; // Extract user and session ID from URL
+    const { user_id, chat_id } = context.params; // Extract user and session ID from URL
 
     if (!user_id) {
       return NextResponse.json({ message: "User ID is required" }, { status: 400 });
