c739e5087496969ebe57d2696ba7e532448f23a6
Daniel Uwadi <danieluwadi@yahoo.com>
2025-03-11 16:19:42 +0100

    Refactored auth options code

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { orderDB } from "@/app/lib/database";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
+import authOptions from "../auth/[...nextauth]/authOptions";
 
 export async function POST(req: Request) {
   const { order } = await req.json();
